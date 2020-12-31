1edf77a012347e7fc875d72df92b3d9041c63ab5
haizim <wahihasyim@gmail.com>
2020-12-31 10:48:20 +0700

    gits

--- git.sh
+++ git.sh
@@ -1,5 +1,8 @@
 git add .
+echo "------------added----------"
 echo "masukkan commit :"
 read commit
 git commit -m "$commit"
+echo "------------committed----------"
 git push -u origin master 
+echo "------------pushed----------"
